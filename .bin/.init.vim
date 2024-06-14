88871c616082f0f316326ec57fe7e1c00bb39c1f
msengnsoni <masaya.satake@sansan.com>
2024-06-15 00:06:08 +0900

    add files

---
+++ .bin/.init.vim
@@ -0,0 +1,55 @@
+" vim basic settings
+" escape noremap
+inoremap <silent> jk <ESC>
+" マウスオーバー
+set mouse=a
+" vi互換の無効
+set nocompatible
+" shellの指定
+set shell=/bin/zsh
+" 画面表示関連
+set ruler
+set showmode
+set showcmd
+set showmatch
+set number
+set laststatus=2
+set virtualedit=onemore
+set wrap
+set linebreak
+set cursorline
+set wildmenu
+" 検索関連
+set hlsearch
+set incsearch
+set smartcase
+set ignorecase
+" タブ/インデント関連
+set autoindent
+set expandtab
+set tabstop=2
+set shiftwidth=2
+" 文字コード関連
+set fenc=utf-8
+set enc=utf-8
+" ビープ関連
+set visualbell
+" 作成ファイル関連
+set noswapfile
+set nobackup
+" ファイル読込関連
+set autoread
+set hidden
+" クリップボードへの登録
+" set clipboard=unnamed
+set ruler
+set showmode
+set showcmd
+set showmatch
+set number
+set laststatus=2
+set virtualedit=onemore
+set wrap
+set linebreak
+set cursorline
+set wildmenu
--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/masaya.satake/.zprofile
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install from .Brewfile
@@ -17,4 +17,17 @@ git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
 
 
+for dotfile in "${SCRIPT_DIR}"/.bin/.??* ; do
+  [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
+  [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
+  [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
+  [[ "$dotfile" == "${SCRIPT_DIR}/.bin/.init.vim" ]] && continue
+  ln -fnsv "$dotfile" "$HOME"
+done
 
+# Check nvim 
+if [ ! -d "$HOME"/.config/nvim ]; then
+  # Create neovim synbolic link
+  mkdir "$HOME"/.config/nvim
+  ln -fnsv "$SCRIPT_DIR"/.bin/.init.vim "$HOME"/.config/nvim/init.vim
+fi
