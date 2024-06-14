db751694eb285852ed7195deb582877ef92750f0
msengnsoni <masaya.satake@sansan.com>
2024-06-14 23:49:55 +0900

    update .Brewfile

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set SCRIPT_DIR
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/masaya.satake/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Install from .Brewfile
+brew bundle --file "${SCRIPT_DIR}/.bin/.Brewfile"
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+
+
+
